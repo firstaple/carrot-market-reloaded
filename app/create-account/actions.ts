016152206663135242311169a41752c5c69076d1
firstaple <firstaple@gmail.com>
2024-04-16 23:34:22 +0900

    Email Log In

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -7,6 +7,7 @@ import { z } from "zod";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import getSession from "@/lib/session";
 
 const checkUsername = (username: string) => !username.includes("potato");
 
@@ -76,7 +77,7 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  const result = await formSchema.safeParseAsync(data);
+  const result = await formSchema.spa(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
@@ -92,14 +93,9 @@ export async function createAccount(prevState: any, formData: FormData) {
         id: true,
       },
     });
-    // save the user to db
-    const cookie = await getIronSession(cookies(), {
-      cookieName: "delicious-karrot",
-      password: process.env.COOKIE_PASSWORD!,
-    });
-    //@ts-ignore
-    cookie.id = user.id;
-    await cookie.save();
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
 
     redirect("/profile");
   }
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,16 +1,36 @@
 "use server";
 
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEC_ERROR, PASSWORD_REGEX } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  // if (user) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
-  password: z
-    .string({
-      required_error: "Password is required",
-    })
-    .min(PASSWORD_MIN_LENGTH)
-    .regex(PASSWORD_REGEX, PASSWORD_REGEC_ERROR),
+  email: z.string().email().toLowerCase().refine(checkEmailExists, "An account with this email does not exist."),
+  password: z.string({
+    required_error: "Password is required",
+  }),
+  // .min(PASSWORD_MIN_LENGTH)
+  // .regex(PASSWORD_REGEX, PASSWORD_REGEC_ERROR),
 });
 
 export async function login(prevState: any, formData: FormData) {
@@ -19,10 +39,34 @@ export async function login(prevState: any, formData: FormData) {
     email: formData.get("email"),
     password: formData.get("password"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.spa(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // if the user is found, check password hash
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    const ok = await bcrypt.compare(result.data.password, user!.password ?? "xxxx");
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["Wrong password."],
+          email: [],
+        },
+      };
+    }
+    // log the user in
+    // redirect "/profile"
   }
 }
